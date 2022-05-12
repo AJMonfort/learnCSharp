b24e124cf278431d594d41a3be679f2c21c4feea
AJMonfort <ajmonfort02@gmail.com>
2022-05-12 13:53:13 -0400

    step1 code

---
+++ learnCSharp/AsyncTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnCSharp
+{
+    public class AsyncTest
+    {
+        public async Task wait10Sec()
+        {
+            Console.WriteLine("Starting Task.");
+            await Task.Delay(5000);
+            Console.WriteLine("Finished Waiting!");
+        }
+
+        public int return5()
+        {
+            return 5;
+        }
+
+        public async Task<int> waitReturn6()
+        {
+            await Task.Delay(1000);
+            return 6;
+        }
+    }
+}
---
+++ learnCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using learnCSharp;
+using System.Threading.Tasks;
+
+Console.WriteLine("Hello, World!");
+
+var test = new AsyncTest();
+Task testTask = test.wait10Sec();
+
+Console.WriteLine("This happened while waiting.");
+Console.WriteLine(test.return5());
+Console.WriteLine(await test.waitReturn6());
+
+await testTask;
+
